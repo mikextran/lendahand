5bc169e574dcd113922ba4dca52b1f75be35fd2f
mikextran <mikextran@yahoo.com>
2016-10-09 19:43:51 -0700

    Mlab Connection

--- config/env/development.js
+++ config/env/development.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://<team>:<lendahand>@ds049486.mlab.com:49486/lend-a-hand-db',
     options: {
-      user: '',
-      pass: ''
+      user: 'team',
+      pass: 'lendahand'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
