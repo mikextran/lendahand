26ff83b9fbc59b2e602005512119ae3c89bb7709
mikextran <mikextran@yahoo.com>
2016-10-09 21:17:41 -0700

    Heroku Test 2

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/lend-a-hand' || || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev' || 'mongodb://<team>:<lendahand>@ds049486.mlab.com:49486/lend-a-hand-db',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://<team>:<lendahand>@ds049486.mlab.com:49486/lend-a-hand-db',
     options: {
       user: 'team',
       pass: 'lendahand'
